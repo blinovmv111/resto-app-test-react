f4bc1564a12be1c099534d9a4f1cb9b923a2380c
blinovmv111 <blinovmv111@gmail.com>
2020-10-30 11:48:49 +0300

    Updates

---
+++ precache-manifest.aafbf34b5ea6409314f7953134f29758.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd2418d8ccbae4c1fd9cfc37f141ba7",
+    "url": "/restoAppFolder/index.html"
+  },
+  {
+    "revision": "21cbab0bd038225d22c8",
+    "url": "/restoAppFolder/static/css/main.8f9ed523.chunk.css"
+  },
+  {
+    "revision": "ec4fd097c8a8cbb11780",
+    "url": "/restoAppFolder/static/js/2.c32ca1cc.chunk.js"
+  },
+  {
+    "revision": "21cbab0bd038225d22c8",
+    "url": "/restoAppFolder/static/js/main.53593dbd.chunk.js"
+  },
+  {
+    "revision": "d097e7954b644c2b45f0",
+    "url": "/restoAppFolder/static/js/runtime~main.243f79e9.js"
+  },
+  {
+    "revision": "698e5e807daf0026f6ac590ce288822c",
+    "url": "/restoAppFolder/static/media/food-bg.698e5e80.jpg"
+  },
+  {
+    "revision": "17087a96763ac04caa08db981aef53ba",
+    "url": "/restoAppFolder/static/media/meat.17087a96.svg"
+  },
+  {
+    "revision": "dc87f2e6b90e66558b321b8963840631",
+    "url": "/restoAppFolder/static/media/pizza.dc87f2e6.svg"
+  },
+  {
+    "revision": "f44865448ebe64a2dadcafadf7317f74",
+    "url": "/restoAppFolder/static/media/salad.f4486544.svg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/restoAppFolder/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
